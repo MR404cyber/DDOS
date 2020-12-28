405a7c3724ddff64c39b1bc759b8709ce0e8dc4f
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2020-12-28 23:35:20 +0800

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -6,7 +6,7 @@ def usage():
     print "\033[1;32m#########################################################"
     print "#------------------------[\033[1;91mLITE-DDOS\033[1;32m]---------------------#"
     print "#-------------------------------------------------------#"
-    print "#   \033[1;91mCommand: " "python2 LITEDDOS.py " "<ip> <port> <packet> \033[1;32m   #"
+    print "#   \033[1;91mCommand: " "python2 ddos.py " "<ip> <port> <packet> \033[1;32m   #"
     print "#                                                       #"
     print "#\033[1;91mCreator: Raihan  \033[1;32m##      ###       ##                #"
     print "#\033[1;91mTeam   : Anonymous       \033[1;32m##     #          ##                #"
